629ee5da9f07d308b2f0efa6e9fc7b1190246b4c
i.lechoev <i.lechoev@zaem.ru>
2025-07-12 23:18:49 +0300

    Добавил кубик, улучшен UI

--- src/apps/places/buttons.py
+++ src/apps/places/buttons.py
@@ -3,7 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 choose_pose_and_location_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="🎯 Выбрать позу и место")]
+        [KeyboardButton(text="🎲 Выбрать позу и место")]
     ],
     resize_keyboard=True
 )
--- src/apps/places/routers.py
+++ src/apps/places/routers.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import F, Router, html
 from aiogram.types import Message
 from src.apps.places.utils import random_generate_pose_and_location
@@ -5,14 +7,16 @@ from src.apps.places.utils import random_generate_pose_and_location
 router = Router()
 
 
-@router.message(F.text == "🎯 Выбрать позу и место")
+@router.message(F.text == "🎲 Выбрать позу и место")
 async def handle_choose_pose_and_location(message: Message) -> None:
     location, pose, description = await random_generate_pose_and_location()
+
+    await message.answer_dice(emoji="🎲")
 
     text = (
         f"{html.bold('🛏️ Место:')} {html.quote(location)}\n"
         f"{html.bold('💋 Поза:')} {html.quote(pose)}\n"
         f"{html.bold('🔥 Описание:')} {html.quote(description)}"
     )
-
+    await asyncio.sleep(4)
     await message.answer(text)
